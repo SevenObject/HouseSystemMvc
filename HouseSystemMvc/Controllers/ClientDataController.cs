4c4aea7518b41d9714a648f11acef65d4d5d1967
zwj85123@163.com <zwj85123@163.com>
2020-09-29 08:42:12 +0800

    0

---
+++ HouseSystemMvc/Controllers/ClientDataController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HouseSystemMvc.Controllers
+{
+    public class ClientDataController : Controller
+    {
+        public IActionResult ShowClientData()
+        {
+            return View();
+        }
+        public IActionResult AddClientData()
+        {
+            return View();
+        }
+    }
+}
