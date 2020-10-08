05c0f644233d28b0e5b594c3ebd4b76235d8e514
zwj85123@163.com <zwj85123@163.com>
2020-10-08 10:32:57 +0800

    55

--- HouseSystemMvc/Startup.cs
+++ HouseSystemMvc/Startup.cs
@@ -46,7 +46,7 @@ namespace HouseSystemMvc
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=HouseInfoContrller}/{action=ShouHouseInfo}/{id?}");
+                    pattern: "{controller=ContractController1}/{action=Login}/{id?}");
             });
         }
     }
