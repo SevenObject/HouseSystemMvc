dd64372ffa071d405e86346a16c5b688f527c231
Administrator <Administrator@WANGBAOYI>
2020-10-08 11:17:04 +0800

    页面修改（2）

--- HouseSystemMvc/Controllers/ContractController.cs
+++ HouseSystemMvc/Controllers/ContractController.cs
@@ -5,9 +5,12 @@ using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Xml;
+using HouseSystemMvc.Views;
 using HouseSystemMvc.Views.Contract;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Session;
 
 namespace HouseSystemMvc.Controllers
 {
@@ -16,6 +19,15 @@ namespace HouseSystemMvc.Controllers
         public IActionResult Login()
         {
             return View();
+        }
+        public ActionResult CheckCode()
+        {
+            //生成验证码
+            ValidateCode validateCode = new ValidateCode();
+            string code = validateCode.CreateValidateCode(4);
+            //HttpContext.Session.SetString("code", code);
+            byte[] bytes = validateCode.CreateValidateGraphic(code);
+            return File(bytes, @"image/jpeg");
         }
         public IActionResult ShowConList()
         {
